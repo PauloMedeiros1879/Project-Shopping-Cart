7bf9a6ad1cf3c7ffd4f7b18e2347f2c8f45a2b83
Paulo_Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-03-28 07:56:02 -0400

    Adiciona requisito 11, tem o retorno errado do 2º test

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -4,6 +4,12 @@ const getSavedCartItems = require('../helpers/getSavedCartItems');
 localStorageSimulator('getItem');
 
 describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('1º - Teste se, ao executar getSavedCartItems, o método localStorage.getItem é chamado', () => {
+    getSavedCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.getItem).toHaveBeenCalled();
+  });
+  it('2º - Teste se, ao executar getSavedCartItems, o método localStorage.getItem é chamado com o "cartItems" como parâmetro', () => {
+    getSavedCartItems('<ol><li>Item</li></ol>');
+    expect(localStorage.getItem).toHaveBeenCalledWith('cartItems');
+  });
 });
